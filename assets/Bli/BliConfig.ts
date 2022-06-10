a40b090ed64a7accf7878792fe7afa6c66f19aaf
DaXing <1148299682@qq.com>
2022-06-10 12:21:01 +0800

    Update BliConfig.ts

--- assets/Bli/BliConfig.ts
+++ assets/Bli/BliConfig.ts
@@ -3,13 +3,13 @@
  */
 export namespace BliConfig {
     /** 开放平台的access_key_id*/
-    export const accessKeyId: string = "vQ2mpFucejdYOx3kWEOifp8l";
+    export const accessKeyId: string = "***********";
     /** 开放平台的access_key_secret */
-    export const accessKeySecret: string = "AkgpF70BuAdlKTObXEj3Sznk9jiPXx";
+    export const accessKeySecret: string = "***********";
     /** 环境域名 */
     export const baseUrl: string = "https://live-open.biliapi.com";
     /** 开放房间的roomId */
-    export const roomId: number = 25044611;
+    export const roomId: number = *********;
     /** 请求ws的地址 */
     export const wsUrl: string = "/v1/common/websocketInfo";
 } 
